49d221d6c4c8e7e887956470bd51388ece6bcc0e
John Carlos Arrieta Arrieta <jarrieta@facturecolombia.com>
2024-02-11 15:39:10 -0500

    Los componentes de la capa Domain (clases POCO tipo entidades, Record tipo ValueObject, Clases tipo Agregados)

---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,7 @@
+namespace App3_ConnBdMySQLDapper.Domain.Entities;
+public class Usuario{
+    public int Id {get; set; }
+    public string Clave {get; set; }
+    public string Nombre {get; set; }
+    public string Email {get; set; }
+}
